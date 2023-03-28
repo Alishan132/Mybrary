0290aefee1139db6fd142616fa7c321366dc6f2d
Alishan Atashev <alishanatasheva@gmail.com>
2023-03-28 20:24:57 +0600

    Initial Author Routes

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ const app = express()
 const dotenv = require("dotenv").config()
 const expressLayouts = require("express-ejs-layouts")
 
-const mongoose = require("mongoose")
+const indexRouter = require("./routes/index")
+const authorsRouter = require("./routes/authors")
 
+
+const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE_URL).then(() => console.log("connected to mongoose"))
 
 app.set("view engine", "ejs")
 app.set("layout", "layouts/layout")
 app.use(expressLayouts)
 app.use(express.static("public"))
+app.use(express.urlencoded({extended: false}))
 
-const indexRouter = require("./routes/index")
 app.use("/", indexRouter)
+app.use("/authors", authorsRouter)
+
+
 
 app.listen(3000, () => console.log("listening on 3000"))
-
-
-
